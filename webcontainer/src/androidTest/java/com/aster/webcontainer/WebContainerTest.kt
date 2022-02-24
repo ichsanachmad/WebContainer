43d6b8a8b84d65bcf8f008c2fdbb2aba78a66a7f
Achmad Ichsan <achmad.ichsan@dana.id>
2022-02-24 22:16:38 +0700

    add instrumentation test

--- webcontainer/src/androidTest/java/com/aster/webcontainer/WebContainerTest.kt
+++ webcontainer/src/androidTest/java/com/aster/webcontainer/WebContainerTest.kt
@@ -3,6 +3,7 @@ package com.aster.webcontainer
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.adevinta.android.barista.assertion.BaristaVisibilityAssertions.assertDisplayed
+import com.adevinta.android.barista.assertion.BaristaVisibilityAssertions.assertNotDisplayed
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -21,6 +22,8 @@ class WebContainerTest {
     @Test
     fun onCall_WebContainer_launch_String_should_open_WebView() {
         WebContainer.launch("www.google.com")
+
         assertDisplayed(R.id.webContainer)
+        assertNotDisplayed(R.id.progressBar)
     }
 }
